c644c0524a54e7ac8d8275890cd33cb52d60224d
Balajis007 <donbalajis007@gmail.com>
2023-08-09 11:23:27 +0530

    update

--- src/main/java/com/fssa/politifact/dao/ConstituencyDao.java
+++ src/main/java/com/fssa/politifact/dao/ConstituencyDao.java
@@ -12,7 +12,13 @@ import com.fssa.politifact.util.ConnectionUtil;
 import com.fssa.politifact.validator.LeaderValidateError;
 import com.fssa.politifact.validator.LeaderValidateException;
 
-public class ConstituencyDao {
+public class ConstituencyDao { 
+	
+	/*
+	 * This insertConstituency methoed only prepare statement code only run hear.
+	 * what reason for that the add leader method codeline i do want to decress so that reson.
+	 * this code add leader prepare statement executeupdate code
+	 */
 
 	private static boolean insertConstituency(Constituency Constituency, PreparedStatement pst) throws SQLException {
 
@@ -42,6 +48,10 @@ public class ConstituencyDao {
 		}
 	}
 
+	/*
+	 * in this method insert update method only prepare statment code heare
+	 * this only update the code return a boolean value
+	 */
 	private static boolean insertUpdate(Constituency Constituency, PreparedStatement pst) throws SQLException {
 
 		pst.setString(1, Constituency.getConstituencyName());
@@ -57,15 +67,22 @@ public class ConstituencyDao {
 		return row > 0;
 
 	}
+	
+	/*
+	 * addConstituency add a new constituency in database 
+	 * this method hava a connection ans also prepare statement this call a insert constituency
+	 * that place run a prepare statement
+	 */
 
 	public static boolean addConstituency(Constituency constituency) throws SQLException, LeaderValidateException {
+		
 		  final String query = "INSERT INTO Constituency (constituencyName, districtName, constituencyNumber, electionTypeId) VALUES (?, ?, ?, ?)";
 
 		try (Connection connection = ConnectionUtil.getConnection()) {
 
 			try (PreparedStatement pst = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
 
-				return insertConstituency(constituency, pst);
+				return insertConstituency(constituency, pst); // invoke the value in to te insert constituency
 
 			} catch (SQLException sqe) {
 
@@ -75,6 +92,11 @@ public class ConstituencyDao {
 			}
 		}
 	}
+	
+	/*
+	 * update constituency perform update the vlaues in database
+	 * this also call a insert update that plass run a prepare Statement
+	 */
 
 	public static boolean updateConstituency(Constituency constituency) throws SQLException, LeaderValidateException {
 
@@ -83,18 +105,24 @@ public class ConstituencyDao {
 		try (Connection connection = ConnectionUtil.getConnection();
 				PreparedStatement pst = connection.prepareStatement(query)) {
 
-			return insertUpdate(constituency, pst);
+			return insertUpdate(constituency, pst);  // invoke the value in insert update methode
+			
 
 		} catch (SQLException sqe) {
 			throw new LeaderValidateException(LeaderValidateError.INVALID_OBJECT);
 		}
 	}
+	
+	/*
+	 * deleteconstituency method deleate a constituency 
+	 * this perform given id
+	 */
 
 	public static boolean deleteConstituency(int constituencyId) throws SQLException, LeaderValidateException {
 
-		final String query = "DELETE FROM constituency WHERE constituencyID=?";
+		final String query = "DELETE FROM constituency WHERE constituencyID=?"; //query
 
-		try (Connection connection = ConnectionUtil.getConnection()) {
+		try (Connection connection = ConnectionUtil.getConnection()) {  // connection
 
 			try (PreparedStatement pst = connection.prepareStatement(query)) {
 
@@ -111,6 +139,13 @@ public class ConstituencyDao {
 			}
 		}
 	}
+	
+	
+	/*
+	 * readAllConstituencies this method connect the database read the all constituency in the table
+	 * and then the store a List and also return
+	 * that return value go to the service layer.
+	 */
 
 	public static ArrayList<Constituency> readAllConstituencies() throws SQLException {
 
@@ -124,6 +159,7 @@ public class ConstituencyDao {
 
 			try (ResultSet rs = stmt.executeQuery(query)) {
 				while (rs.next()) {
+					
 					Constituency constituency = new Constituency( "constituency", "district", 1, 1);
 
 					constituency.setConstituencyName(rs.getString(2));
@@ -138,7 +174,6 @@ public class ConstituencyDao {
 
 			} catch (SQLException sqe) {
 
-				System.out.println(sqe.getMessage());
 				return null;
 			}
 		}
